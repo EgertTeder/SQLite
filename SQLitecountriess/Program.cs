4ffbde0d767deb9fe418f10e4e0f8bf2a6eb49ab
Opilane TTHK <opilane@tthk.ee>
2022-02-18 11:33:23 +0200

    sqlite

---
+++ SQLitecountriess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQLitecountriess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
